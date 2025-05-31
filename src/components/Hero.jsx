fbfe079108d4d4f02da33890059c68643589aebb
jangamnoelpaulmoses <jangamnoelpaulmoses@gmail.com>
2025-05-30 21:17:00 -0700

    hide scroll bar prompt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -221,7 +221,7 @@ const Hero = () => {
           </div>
 
       {/* Scroll Prompt */}
-      <div className="absolute xs:bottom-8 bottom-20 w-full flex justify-center items-center md:hidden ">
+      <div className="absolute xs:bottom-8 bottom-20 w-full flex justify-center items-center sm:hidden ">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
